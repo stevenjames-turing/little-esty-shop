f9136c98342230b4007005c47b083bf662dd1ee2
Tyler Ross <tross020890@gmail.com>
2022-02-20 17:04:13 -0600

    Feat: add admin merchants, invoices

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get '/merchants/:id/dashboard', to: 'merchants#show'
   get '/admin', to: 'admin#show'
+  get '/admin/merchants', to: 'admin_merchants#index'
+  get '/admin/invoices', to: 'admin_invoices#index'
 end
