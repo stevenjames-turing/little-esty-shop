e1f7c01686685808dbd143809452cf64dba25f4b
Tyler Ross <tross020890@gmail.com>
2022-02-20 20:25:51 -0600

    Feat: admin dash shows top customers (incorrectly)

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,20 @@
 class Admin < ApplicationRecord
+has_many :customers
+
+def self.top_customers(count)
+  customer = Customer.find_by_sql("SELECT customers.* FROM customers
+                        JOIN invoices ON customers.id = invoices.customer_id
+                        JOIN invoice_items ON invoices.id = invoice_items.invoice_id
+                        JOIN items ON invoice_items.item_id = items.id
+                        JOIN merchants ON items.merchant_id = merchants.id
+                        JOIN transactions on transactions.invoice_id = invoices.id
+
+                        WHERE transactions.result = 0
+
+                        GROUP BY customers.id
+                        ORDER BY COUNT(transactions.id) DESC
+
+                        LIMIT #{count}")
+end
 
 end
