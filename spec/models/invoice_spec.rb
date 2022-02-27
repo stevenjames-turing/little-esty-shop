3bc8314a2ec48adeb834a8de7c9a26af4cbcf176
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-02-27 15:55:50 -0600

    Refactor Invoice model test to add describe block

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,26 +8,29 @@ RSpec.describe Invoice, type: :model do
   it {should belong_to :customer}
 
   describe 'instance methods' do
-    it 'can reformat the created_at timestamp' do
-      invoice_1 = create(:invoice, created_at: Time.parse("2022-02-22"))
-
-      expect(invoice_1.created_at_date).to eq("Tuesday, February 22, 2022")
+    describe '.created_at_date' do 
+      it 'can reformat the created_at timestamp' do
+        invoice_1 = create(:invoice, created_at: Time.parse("2022-02-22"))
+  
+        expect(invoice_1.created_at_date).to eq("Tuesday, February 22, 2022")
+      end
     end
-
-    it 'can calculate the total revenue of each merchant' do
-      merchant_1 = create(:merchant)
-      customer_1 = create(:customer)
-      item_1 = create(:item, merchant_id: merchant_1.id)
-      invoice_1 = create(:invoice, customer_id: customer_1.id)
-      invoice_item_1 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id, unit_price: 3, quantity: 1)
-
-      item_2 = create(:item, merchant_id: merchant_1.id)
-      invoice_item_2 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_1.id, unit_price: 5, quantity: 1)
-
-      item_3 = create(:item, merchant_id: merchant_1.id)
-      invoice_item_3 = create(:invoice_item, item_id: item_3.id, invoice_id: invoice_1.id, unit_price: 4, quantity: 2)
-
-      expect(invoice_1.total_revenue).to eq(16)
+    describe '.total_revenue' do 
+      it 'can calculate the total revenue of each merchant' do
+        merchant_1 = create(:merchant)
+        customer_1 = create(:customer)
+        item_1 = create(:item, merchant_id: merchant_1.id)
+        invoice_1 = create(:invoice, customer_id: customer_1.id)
+        invoice_item_1 = create(:invoice_item, item_id: item_1.id, invoice_id: invoice_1.id, unit_price: 3, quantity: 1)
+  
+        item_2 = create(:item, merchant_id: merchant_1.id)
+        invoice_item_2 = create(:invoice_item, item_id: item_2.id, invoice_id: invoice_1.id, unit_price: 5, quantity: 1)
+  
+        item_3 = create(:item, merchant_id: merchant_1.id)
+        invoice_item_3 = create(:invoice_item, item_id: item_3.id, invoice_id: invoice_1.id, unit_price: 4, quantity: 2)
+  
+        expect(invoice_1.total_revenue).to eq(16)
+      end
     end
   end
 end
