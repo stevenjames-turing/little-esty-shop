d92c7a5203368c923e1978fbababfea94331c20b
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-03-03 16:24:24 -0600

    Resolve merge conflicts

--- spec/factories/invoice.rb
+++ spec/factories/invoice.rb
@@ -1,10 +1,6 @@
 FactoryBot.define do
   factory :invoice, class: Invoice do
-<<<<<<< HEAD
     status { (["completed", "cancelled", "in progress"]).sample(1)[0] }
-=======
-    status { (['completed', 'cancelled', 'in progress']).sample(1)[0] } # What should we pass?
->>>>>>> e23c6f47c5743939519913e030fb975813633a5d
     customer
   end
 end
